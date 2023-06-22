1e493234be550d4cb7a44f73b012bec3e5bd2a98
JoshB <Joshoboy4@gmail.com>
2023-06-22 13:00:33 +0100

    my third commit

---
+++ Javascript_tutorials.js
@@ -0,0 +1,14 @@
+var database[{
+    username: "joshoboy",
+    password: "mysecre"
+}];
+
+var newsFeed[{
+    username: "Bobby",
+    timeline: "I am so tired of learning Javascript"
+} {
+    username: "Cherish",
+    timeline: "Please always give me chocolate i will be fine"
+}];
+var userNamePrompt = Prompt("Enter your Username");
+var passwordPrompt = Prompt("Enter your Password");
